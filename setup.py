f1c3d3a08bed87d58dbf3f22d0ebbde254685d18
Michael Verhulst <michael@terminallabs.com>
2020-10-26 23:11:24 -0500

    added in some pin deps

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
 from distutils.core import setup
+
+pins = [
+    "setuptools<=45",
+    "PySocks!=1.5.7,>=1.5.6",
+    "Werkzeug<1",
+    "tornado<5.0,>=4.2.1",
+    "pyzmq<17.1.0",
+    "salt==2018.3.3",
+    "ansible==2.9.0",
+]
+
 setup(
   name = 'standardmodel',
   packages = ['standardmodel'],
@@ -8,15 +19,10 @@ setup(
   author_email="solutions@terminallabs.com",
   url = 'https://github.com/terminal-labs/standardmodel',
   download_url = 'https://github.com/terminal-labs/standardmodel/archive/master.zip',
-  install_requires=[
-    "setuptools<=45",
+  install_requires=pins + [
     "utilities-package@git+https://gitlab.com/terminallabs/utilitiespackage/utilities-package.git",
-    "utilities-package_cli-metapackage@git+https://gitlab.com/terminallabs/utilitiespackage/metapackages/utilities-package_cli-metapackage.git@master#egg=utilitiespackageclimetapackage&subdirectory=utilitiespackageclimetapackage",
+    "utilities-package_cli-metapackage@git+https://gitlab.com/terminallabs/utilitiespackage/metapackages/utilities-package_cli-metapackage.git",
     "rambo-vagrant@git+https://github.com/terminal-labs/rambo.git",
-    "tornado<5.0,>=4.2.1",
-    "pyzmq<17.1.0",
-    "salt==2018.3.3",
-    "ansible==2.9.0",
   ],
   classifiers=[  # Optional
     'Development Status :: 3 - Alpha',
